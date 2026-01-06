0d1c97d6c426979cdb1b73aabed9da228be11368
Dio Douglas Nsubuga <diodouglasnsubuga256@gmail.com>
2026-01-06 20:58:23 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Placeholder for future JS functionality
+console.log("Gofast Car Bond website loaded");
+
+// Example: Alert when someone clicks a call button
+document.querySelectorAll('.car-item p').forEach(item => {
+    item.addEventListener('click', () => {
+        console.log("Contact info clicked");
+    });
+});
